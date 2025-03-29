32415d1332b783459d30e2a6890f963b9daf9d64
shank227 <shashank.bakshi03@gmail.com>
2025-03-29 11:16:56 +0530

    week1: task1

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, redirect
+import redis
+import hashlib
+
+app = Flask(__name__)
+
+redis_client = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+
+BASE_URL = "http://localhost:5000/"
+
+def generate_short_url(long_url):
+    hash_object = hashlib.md5(long_url.encode())
+    return hash_object.hexdigest()[:6]  
+
+@app.route('/shorten', methods=['POST'])
+def shorten_url():
+    data = request.get_json()
+    long_url = data.get("long_url")
+    
+    if not long_url:
